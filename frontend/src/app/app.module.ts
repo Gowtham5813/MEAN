5ebd7700c9e700558558bff9eca9a5ac19966176
Gowtham5813 <gouthamavala@gmail.com>
2024-10-10 15:04:23 +0530

    images and navbar created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Component/footer/footer.component';
+import { NavbarComponent } from './Component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
