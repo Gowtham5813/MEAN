ebebb09325fbcff0fc280572685d5cb8100da64f
Gowtham5813 <gouthamavala@gmail.com>
2024-10-15 10:53:50 +0530

    created left and right components to the page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingpageComponent } from './Component/landingpage/landingpage.component';
+import { SelectbusPageComponent } from './Component/selectbus-page/selectbus-page.component';
+
 const routes: Routes = [
-  {path:'',component:LandingpageComponent}
+  {path:'',component:LandingpageComponent},
+  {path:'select-bus',component:SelectbusPageComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './Component/footer/footer.component';
 import { NavbarComponent } from './Component/navbar/navbar.component';
 import { LandingpageComponent } from './Component/landingpage/landingpage.component';
+import { SelectbusPageComponent } from './Component/selectbus-page/selectbus-page.component';
+import { HeaderComponent } from './Component/selectbus-page/header/header.component';
+import { LeftComponent } from './Component/selectbus-page/left/left.component';
+import { RightComponent } from './Component/selectbus-page/right/right.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     NavbarComponent,
-    LandingpageComponent
+    LandingpageComponent,
+    SelectbusPageComponent,
+    HeaderComponent,
+    LeftComponent,
+    RightComponent
   ],
   imports: [
     BrowserModule,
